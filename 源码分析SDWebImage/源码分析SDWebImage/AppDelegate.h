fc190f8f174e1c187d7f30d703519e063d3ed872
joey_qi <joey_qi@Wolianw-iOS-6deiMac.local>
2017-09-26 09:20:03 +0800

    1. 创建本地工程。

---
+++ 源码分析SDWebImage/源码分析SDWebImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  源码分析SDWebImage
+//
+//  Created by joey_qi on 2017/9/26.
+//  Copyright © 2017年 joey_qi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
